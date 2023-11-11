b7a6be01aa973ce8c64db276a819828d9dc538c1
incyberz <incyberz@yahoo.com>
2023-11-11 14:54:11 +0700

    first commit

---
+++ js/my-script.js
@@ -0,0 +1,11 @@
+function alertExternal() {
+  alert("Hallo! Ini alert Javascript secara eksternal.");
+  alert(
+    "Kelebihan secara eksternal: " +
+      "\n- dapat lebih dari satu perintah; " +
+      "\n- bisa digunakan di semua pages; " +
+      "\n- dan code terlihat lebih simple; " +
+      "\n- dan cocok untuk kerja tim."
+  );
+  confirm("Sangat recommended!");
+}
